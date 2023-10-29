6e7be361cc8e29513e5e261478062b2168fa3765
Paplavskaya <any-lion@mail.ru>
2023-10-29 10:19:39 +0300

    feat - added atribute lang and tags meta, title, link

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const html = document.querySelector('html');
+html.setAttribute("lang", 'en');
+
+const head = document.querySelector('head');
+head.innerHTML = `
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Call to Action</title>
+                <link rel="stylesheet" href="./css/style.css">
+`
