a378d4bbbb9e0a3ac7828ff8b98b58f8cb38e3d2
Paplavskaya <any-lion@mail.ru>
2023-10-29 11:15:25 +0300

    feat - added element optionHeaderTitle, optionHeaderSubTitle

--- js/script.js
+++ js/script.js
@@ -26,3 +26,12 @@ const optionItems = document.createElement('div');
 optionItems.classList.add('option__items');
 option.appendChild(optionItems);
 
+const optionHeaderTitle = document.createElement('h2');
+optionHeaderTitle.classList.add('option__header__title');
+optionHeaderTitle.textContent = 'Choose Your Option';
+optionHeader.appendChild(optionHeaderTitle);
+
+const optionHeaderSubTitle = document.createElement('span');
+optionHeaderSubTitle.classList.add('option__header__sub__title');
+optionHeaderSubTitle.textContent = 'But I must explain to you how all this mistaken idea of denouncing';
+optionHeader.appendChild(optionHeaderSubTitle);
